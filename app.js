417d0b9c57d9bacc270285e413a14d3bc16402c7
Vladislav <www.vladtarna25@gmail.com>
2023-12-27 22:15:27 +0200

    +

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('openBtn').addEventListener('click', function() {
+  document.getElementById('menu-mob').style.display = 'flex';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+  document.getElementById('menu-mob').style.display = 'none';
+});
